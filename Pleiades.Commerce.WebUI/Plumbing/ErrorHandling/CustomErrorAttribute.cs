1109f06dd0274532db026ccf008b6f996c9f6729
Aleks Jones <aleksjones@gmail.com>
2012-03-11 15:19:16 -0500

    Renmaed old extension method classes

---
+++ Pleiades.Commerce.WebUI/Plumbing/ErrorHandling/CustomErrorAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pleiades.Commerce.WebUI.Plumbing.ErrorHandling
+{
+    public class CustomErrorAttribute :  HandleErrorAttribute
+    {
+        // Summary:
+        //     Called when an exception occurs.
+        //
+        // Parameters:
+        //   filterContext:
+        //     The action-filter context.
+        //
+        // Exceptions:
+        //   System.ArgumentNullException:
+        //     The filterContext parameter is null.
+        public virtual void OnException(ExceptionContext filterContext)
+        {
+            Debug.WriteLine(filterContext.Exception.Message);
+        }
+    }
+}
