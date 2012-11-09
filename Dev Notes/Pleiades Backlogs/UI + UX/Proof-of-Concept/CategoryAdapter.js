186e40c1814406213087bedadc9f7e43776e2402
Aleks Jones <aleksjones@gmail.com>
2012-11-09 08:28:19 -0600

    MILESTONE - KEEP PUSHING!!!

--- Dev Notes/Pleiades Backlogs/UI + UX/Proof-of-Concept/CategoryAdapter.js
+++ Dev Notes/Pleiades Backlogs/UI + UX/Proof-of-Concept/CategoryAdapter.js
@@ -1,127 +1,113 @@
 
 // I WANT SYMMETRY!  I WANT TO MAKE ASSEMBLING STUFF EASY FOR THE SERVER.  <=== FUCK YOU!!!  BUILDING JSON IS TRIVIAL FOR C#
+// UPDATE: everything was upside down.  This should store everything flat for ease-of-manipulation, then translate into JSON accordingly.
+// THEREFORE: refactor this and eliminate the tree structure bullshit in our mock data layer.
+
 
 function CategoryDataAdapter() {
 	var self = this;
-	self.Categories = [
-		{ 
-			Id: "3", Name: "Shoes", SEO: "sample-seo-text", 
-			Categories: [
-				{ ParentId: "3", Id: "4", Name: "Golf Shoes", SEO: "sample-seo-text-4" },
-				{ ParentId: "3", Id: "5", Name: "Playah Shoes", SEO: "sample-seo-text-5" },
-				{ ParentId: "3", Id: "6", Name: "Mountain Shoes", SEO: "sample-seo-text-6" },
-				{ ParentId: "3", Id: "7", Name: "Ass-Kicking Shoes", SEO: "sample-seo-text-7" },
-			]
-		},
-		{
-			Id: "8", Name: "Jiu-jitsu Gear", SEO: "sample-seo-text-8",
-			Categories: [
-				{ ParentId: "8", Id: "9", Name: "Choke-proof Gis", SEO: "sample-seo-text-9" },
-				{ ParentId: "8", Id: "10", Name: "Belts", SEO: "sample-seo-text-10" },
-				{ ParentId: "8", Id: "11", Name: "Mouth Guards", SEO: "sample-seo-text-11" } ,
-			]
-		},
-		{
-			Id: "12", Name: "Helmets", SEO: "sample-seo-text-12",
-			Categories: [
-				{ ParentId: "12", Id: "13", Name: "Open Face", SEO: "sample-seo-text-13" },
-				{ ParentId: "12", Id: "14", Name: "Vintage", SEO: "sample-seo-text-14" },
-				{ ParentId: "12", Id: "15", Name: "Viking", SEO: "sample-seo-text-15" },
-				{ ParentId: "12", Id: "16", Name: "Samurai", SEO: "sample-seo-text-16" },
-			],
-		},
-		{
-			Id: "17", Name: "Boxing", SEO: "sample-seo-text-17",
-			Categories: [
-				{ ParentId: "17", Id: "18", Name: "Punching Bags", SEO: "sample-seo-text-18" },
-				{ ParentId: "17", Id: "19", Name: "Gloves", SEO: "sample-seo-text-19" },
-				{ ParentId: "17", Id: "20", Name: "Mouth Guards", SEO: "sample-seo-text-20" },
-				{ ParentId: "17", Id: "21", Name: "Shorts", SEO: "sample-seo-text-21" },
-			],
-		},
-		{
-			Id: "22", Name: "Paddy Cake", SEO: "sample-seo-text-22",
-			Categories: [
-				{ ParentId: "22", Id: "21", Name: "Tea", SEO: "sample-seo-text-22" },
-				{ ParentId: "22", Id: "22", Name: "Spice", SEO: "sample-seo-text-23" },
-				{ ParentId: "22", Id: "23", Name: "Everything Nice", SEO: "sample-seo-text-23" },
-				{ ParentId: "22", Id: "24", Name: "Crumpets", SEO: "sample-seo-text-24" },
-			],
-		},
+	self.DataStore = [
+		{ ParentId: null, Id: "3", Name: "Shoes", SEO: "sample-seo-text" },
+		{ ParentId: "3", Id: "4", Name: "Golf Shoes", SEO: "sample-seo-text-4" },
+		{ ParentId: "3", Id: "5", Name: "Playah Shoes", SEO: "sample-seo-text-5" },
+		{ ParentId: "3", Id: "6", Name: "Mountain Shoes", SEO: "sample-seo-text-6" },
+		{ ParentId: "3", Id: "7", Name: "Ass-Kicking Shoes", SEO: "sample-seo-text-7" },
+		{ ParentId: null, Id: "8", Name: "Jiu-jitsu Gear", SEO: "sample-seo-text-8" },
+		{ ParentId: "8", Id: "9", Name: "Choke-proof Gis", SEO: "sample-seo-text-9" },
+		{ ParentId: "8", Id: "10", Name: "Belts", SEO: "sample-seo-text-10" },
+		{ ParentId: "8", Id: "11", Name: "Mouth Guards", SEO: "sample-seo-text-11" } ,
+		{ ParentId: null, Id: "12", Name: "Helmets", SEO: "sample-seo-text-12" },
+		{ ParentId: "12", Id: "13", Name: "Open Face", SEO: "sample-seo-text-13" },
+		{ ParentId: "12", Id: "14", Name: "Vintage", SEO: "sample-seo-text-14" },
+		{ ParentId: "12", Id: "15", Name: "Viking", SEO: "sample-seo-text-15" },
+		{ ParentId: "12", Id: "16", Name: "Samurai", SEO: "sample-seo-text-16" },
+		{ ParentId: null, Id: "17", Name: "Boxing", SEO: "sample-seo-text-17" },
+		{ ParentId: "17", Id: "18", Name: "Punching Bags", SEO: "sample-seo-text-18" },
+		{ ParentId: "17", Id: "19", Name: "Gloves", SEO: "sample-seo-text-19" },
+		{ ParentId: "17", Id: "20", Name: "Mouth Guards", SEO: "sample-seo-text-20" },
+		{ ParentId: "17", Id: "21", Name: "Shorts", SEO: "sample-seo-text-21" },
+		{ ParentId: null, Id: "22", Name: "Paddy Cake", SEO: "sample-seo-text-22" },
+		{ ParentId: "22", Id: "23", Name: "Tea", SEO: "sample-seo-text-22" },
+		{ ParentId: "22", Id: "24", Name: "Spice", SEO: "sample-seo-text-23" },
+		{ ParentId: "22", Id: "25", Name: "Everything Nice", SEO: "sample-seo-text-23" },
+		{ ParentId: "22", Id: "26", Name: "Crumpets", SEO: "sample-seo-text-24" },
 	];
-	
-	self.Find = function(id) { 
-		var output = self.FindAll(function(node) { return node.Id === id });
-		return output[0];
+		
+	// These belong in the Common library
+	self.FindById = function(id) { 
+		var output = self.FindAll(function(element) { return element.Id === id });
+		if (output.length == 0)
+			return null;
+		else
+			return output[0];
 	}
 	
-	self.FindParent = function(id) {
-		var output = [];
-		var lambda = function(node) { 
-			if (node.Categories) {
-				var index = node.Categories.indexOf(function(node2) { return node2.Id === id; } );
-				return index != -1;  
-			} 
-			else {
-				return false;
-			}
-		};
-		self.FindWorker(self, lambda, output);
-		return output[0];
+	self.FindByParentId = function(parentId) {
+		var output = self.FindAll(function(element) { return element.ParentId === parentId });
+		return output;
 	}
 	
 	self.FindAll = function(lambda) {
 		var output = [];
-		self.FindWorker(self, lambda, output);
+		$.each(self.DataStore, function(index, element) { if (lambda(element) === true) { output.push(element); } });
 		return output;
 	};
-	
-	self.FindWorker = function(node, lambda, output) {
-		if (lambda(node)) {
-			output.push(node);
-		}
-		if (node.Categories) {
-			$.each(node.Categories, function(index, element) { self.FindWorker(element, lambda, output); });
-		}
-	};
-	
-	self.RetrieveAllCategories = function () {
-		var output = [];
-		$.each(self.Categories, function(index, element) { output.push(DeepClone(element)); });
-		return output;
+		
+	self.RetrieveById = function (id) {
+		var category = DeepClone(self.FindById(id));
+		category.Categories = [];
+		
+		var children = self.FindByParentId(id);
+		$.each(children, function(index, element) { 
+			category.Categories.push(self.RetrieveById(element.Id)); 
+		});
+		
+		return category;
 	}
 	
-	self.RetrieveSingleCategory = function (id) {
-		return DeepClone(self.Find(id));
+	self.RetrieveByParentId = function (parentId) {
+		var categories = self.FindByParentId(parentId);
+		var output = [];
+		$.each(categories, function(index, element) {			
+			output.push(self.RetrieveById(element.Id)); 
+		});
+		return output;
+	}
+
+	self.RetrieveAll = function () {
+		return self.RetrieveByParentId(null);
 	}
 	
 	self.Delete = function(id) {
-		var parent = self.FindParent(id);
-		if (parent) {
-			parent.Categories.removeByLambda( function(node) { return node.Id === id; });
-		}
+		self.DataStore.removeByLambda(function(element) { return element.Id === id; });
 	}
 	
-	// To support out madness, it's necessary to make this more sophisticated.  Ok?  Ok.
-	self.SaveSingleCategory = function (category) {				
+	self.Save = function(category) {
 		if (category.Id == null) {
+			// TODO: check for wrong ParentId 123
 			var newId = (100 + Math.floor(Math.random() * 11)).toString();
-			category.Id = newId;				
-			var parent = self.FindParent(category.ParentId);
-			parent.push(category);
-			return data.Id;
-		} 
-		else {
-			var existingCategory = self.Find(category.Id);
-			existingCategory.Name = category.Name;
-			existingCategory.SEO = category.SEO;
 			
-			if (existingCategory.ParentId !== category.ParentId) {
-				self.Delete(category.Id);
-				existingCategory.ParentId = category.ParentId;
-				var parent = self.Find(category.ParentId);
-				parent.Categories.push(existingCategory);
+			self.DataStore.push({
+				Id: newId,
+				ParentId: category.ParentId,
+				Name: category.Name,
+				SEO: category.SEO,
+			});
+			
+			return newId;
+		}
+		else {
+			if (category.Id === category.ParentId) {
+				throw "Oh noes! Infinite loop!";
 			}
+			
+			var persistCategory = self.FindById(category.Id);
+			persistCategory.ParentId = category.ParentId;
+			persistCategory.Name = category.Name;
+			persistCategory.SEO = category.SEO;
+			
+			return persistCategory.Id;
 		}
 	}
 }
+
